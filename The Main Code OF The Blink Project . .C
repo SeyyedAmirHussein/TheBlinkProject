e1e6f71f03aea68e53a7b25526000bf791a35dd0
SeyyedAmirHussein <148333552+SeyyedAmirHussein@users.noreply.github.com>
2023-11-09 19:41:43 +0330

    Create The Main Code OF The Blink Project . .C

---
+++ The Main Code OF The Blink Project . .C
@@ -0,0 +1,28 @@
+// The Blink Project .
+
+// The Main Code OF The Blink Project . .C
+
+// In this program, We wrote a Flashing Light program using an Arduino UNO Board and an LED .
+
+
+
+
+// Determine The Bases .
+
+
+int LED_pin = 7;                                                                                                                                // Definition of Different Bases .
+
+
+// The Setup Function runs once when you Press Reset or Power The Board .
+
+
+void setup () {
+
+
+  // Initialize Digital pin LED_pin as an OUTPUT .
+
+
+  pinMode (LED_pin, OUTPUT);                                                                                                          // Introducing The OUTPUT Pin .
+
+
+}
